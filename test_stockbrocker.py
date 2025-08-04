b08dbabaada0530d0983e15b3e4f72a21910a737
yuhyeok <joyu0320@naver.com>
2025-08-04 15:15:19 +0900

    [feature] fix unittest

--- test_stockbrocker.py
+++ test_stockbrocker.py
@@ -51,14 +51,14 @@ def test_login_nemo_mock(mocker: MockerFixture, user_info):
 
 def test_login_kiwer_api(user_info):
     api = "kiwer"
-    driver = AutoTradingSystem(api, KiwerDriver)
+    driver = AutoTradingSystem(api, AutoTradingSystem)
     driver.select_stock_brocker(api)
     assert driver.login(user_info.user_id, user_info.password) == f"[{api}] {user_info.user_id} login success"
 
 
 def test_login_nemo_api(user_info):
     api = "nemo"
-    driver = AutoTradingSystem(api, NemoDriver)
+    driver = AutoTradingSystem(api, AutoTradingSystem)
     driver.select_stock_brocker(api)
     assert driver.login(user_info.user_id, user_info.password) == f"[{api}] {user_info.user_id} login success"
 
@@ -77,26 +77,26 @@ def stock_info(mocker):
 
 
 def test_buy_mock(mocker: MockerFixture, stock_info):
-    driver = mocker.Mock(spec=AutoTradingSystem)
+    driver = mocker.Mock(spec=StockBrokerDriverInterface)
     api = "mock"
-    driver.select_stock_brocker(api)
-    driver.buy.return_value = f"[{api}] {stock_info.code}, {stock_info.price}, {stock_info.counts} buy success"
-    assert driver.buy(stock_info.code, stock_info.price,
+    auto_trading_system = AutoTradingSystem(api, driver)
+    driver.buy.return_value = True
+    assert auto_trading_system.buy(stock_info.code, stock_info.price,
                       stock_info.counts) == f"[{api}] {stock_info.code}, {stock_info.price}, {stock_info.counts} buy success"
 
 
 def test_sell_mock(mocker: MockerFixture, stock_info):
-    driver = mocker.Mock(spec=AutoTradingSystem)
+    driver = mocker.Mock(spec=StockBrokerDriverInterface)
     api = "mock"
-    driver.select_stock_brocker(api)
-    driver.sell.return_value = f"[{api}] {stock_info.code}, {stock_info.price}, {stock_info.counts} sell success"
-    assert driver.sell(stock_info.code, stock_info.price,
+    auto_trading_system = AutoTradingSystem(api, driver)
+    driver.sell.return_value = True
+    assert auto_trading_system.sell(stock_info.code, stock_info.price,
                        stock_info.counts) == f"[{api}] {stock_info.code}, {stock_info.price}, {stock_info.counts} sell success"
 
 
 def test_get_price_mock(mocker: MockerFixture, stock_info):
-    driver = mocker.Mock(spec=AutoTradingSystem)
+    driver = mocker.Mock(spec=StockBrokerDriverInterface)
     api = "mock"
-    driver.select_stock_brocker(api)
+    auto_trading_system = AutoTradingSystem(api, driver)
     driver.get_price.return_value = 30000
-    assert driver.get_price(stock_info.code) == 30000
+    assert auto_trading_system.get_price(stock_info.code) == 30000
