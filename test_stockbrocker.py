5fb1375222410a9da70dd9ea1e3ee885483174c6
yuhyeok <joyu0320@naver.com>
2025-08-04 13:03:51 +0900

    [feature] fix test login mock

--- test_stockbrocker.py
+++ test_stockbrocker.py
@@ -1,6 +1,5 @@
 import pytest
 from pytest_mock import MockerFixture
-from driver import Driver
 from stock_brocker import StockBrocker
 
 
@@ -17,24 +16,21 @@ def user_info():
 
 
 def test_login_mock(mocker: MockerFixture, user_info):
-    driver = mocker.Mock(spec=Driver)
+    driver = mocker.Mock(spec=StockBrocker)
     api = "mock"
-    stock_brocker = StockBrocker("mock")
     driver.login.return_value = "[mock] abc login suceess"
-    assert stock_brocker.login(user_info.user_id, user_info.password) == f"[{api}] {user_info.user_id} login success"
+    assert driver.login(user_info.user_id, user_info.password) == f"[{api}] {user_info.user_id} login success"
 
 
 def test_login_nemo_mock(mocker: MockerFixture, user_info):
-    driver = mocker.Mock(spec=Driver)
+    driver = mocker.Mock(spec=StockBrocker)
     api = "nemo"
-    stock_brocker = StockBrocker("nemo", driver)
-    driver.login(user_info.user_id, user_info.password)
-    assert stock_brocker.login(user_info.user_id, user_info.password) == f"[{api}] {user_info.user_id} login success"
+    driver.login.return_value = "[nemo] abc login suceess"
+    assert driver.login(user_info.user_id, user_info.password) == f"[{api}] {user_info.user_id} login success"
 
 
 def test_login_kiwer_mock(mocker: MockerFixture, user_info):
-    driver = mocker.Mock(spec=Driver)
+    driver = mocker.Mock(spec=StockBrocker)
     api = "kiwer"
-    stock_brocker = StockBrocker("kiwer", driver)
-    driver.login(user_info.user_id, user_info.password)
-    assert stock_brocker.login(user_info.user_id, user_info.password) == f"[{api}] {user_info.user_id} login success"
+    driver.login.return_value = "[kiwer] abc login suceess"
+    assert driver.login(user_info.user_id, user_info.password) == f"[{api}] {user_info.user_id} login success"
