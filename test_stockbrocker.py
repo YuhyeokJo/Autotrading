3d89b8fb15fd77a003625c5ffa0a514eaa7982a4
yuhyeok <joyu0320@naver.com>
2025-08-04 10:58:30 +0900

    [feature] add kiwer login

--- test_stockbrocker.py
+++ test_stockbrocker.py
@@ -1,6 +1,7 @@
 import pytest
 from pytest_mock import MockerFixture
 
+from kiwer_api import KiwerAPI
 from nemo_api import NemoAPI
 from stock_brocker import StockBrocker
 
@@ -13,3 +14,14 @@ def test_login_nemo_api(mocker: MockerFixture):
     stock_brocker = StockBrocker("nemo", driver)
     driver.login(id, passward)
     assert stock_brocker.login(id, passward) == f"[{api}] {id} login success"
+
+
+def test_login_kiwer_api(mocker: MockerFixture):
+    driver = mocker.Mock(spec=KiwerAPI)
+    api = "kiwer"
+    id = "abc"
+    passward = "1111"
+    stock_brocker = StockBrocker("kiwer", driver)
+    driver.login(id, passward)
+    assert stock_brocker.login(id, passward) == f"[{api}] {id} login success"
+
