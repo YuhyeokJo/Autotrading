165970c20ece8516a90fd8eee2a7e40748dec419
yuhyeok <joyu0320@naver.com>
2025-08-04 10:38:09 +0900

    [feature] test nemo login

---
+++ stock_brocker.py
@@ -0,0 +1,2 @@
+class StockBrocker:
+    pass
--- test_stockbrocker.py
+++ test_stockbrocker.py
@@ -1,2 +1,14 @@
-def test_():
-    ...
+import pytest
+from pytest_mock import MockerFixture
+
+from nemo_api import NemoAPI
+from stock_brocker import StockBrocker
+
+
+def test_login_nemo_api(mocker: MockerFixture):
+    driver = mocker.Mock(spec=NemoAPI)
+    stock_brocker = StockBrocker(driver)
+    id = "abc"
+    passward = "1111"
+    driver.login(id, passward)
+    assert stock_brocker.login("nemo", id, passward) == f"{id} login success"
