0eefa12da843d3e2be2a2389fdcdb9eec77d00c0
jihyung <lifehappy@naver.com>
2025-08-04 15:36:16 +0900

    [feature] buy, sell, get_price StockBroderDriverInterface 추가

--- test_stockbrocker.py
+++ test_stockbrocker.py
@@ -100,3 +100,96 @@ def test_get_price_mock(mocker: MockerFixture, stock_info):
     auto_trading_system = AutoTradingSystem(api, driver)
     driver.get_price.return_value = 30000
     assert auto_trading_system.get_price(stock_info.code) == 30000
+
+
+
+def test_sell_nemo_mock(mocker: MockerFixture, stock_info):
+    driver = mocker.Mock(spec=StockBrokerDriverInterface)
+    api = "nemo"
+    auto_trading_system = AutoTradingSystem(api, driver)
+    driver.sell.return_value = True
+    assert auto_trading_system.sell(stock_info.code, stock_info.price, stock_info.counts) == f"[{api}] {stock_info.code}, {stock_info.price}, {stock_info.counts} sell success"
+
+
+def test_sell_kiwer_mock(mocker: MockerFixture, stock_info):
+    driver = mocker.Mock(spec=StockBrokerDriverInterface)
+    api = "kiwer"
+    auto_trading_system = AutoTradingSystem(api, driver)
+    driver.sell.return_value = True
+    assert auto_trading_system.sell(stock_info.code, stock_info.price, stock_info.counts) == f"[{api}] {stock_info.code}, {stock_info.price}, {stock_info.counts} sell success"
+
+
+def test_sell_kiwer_api(stock_info):
+    api = "kiwer"
+    driver = AutoTradingSystem(api, AutoTradingSystem)
+    driver.select_stock_brocker(api)
+    assert driver.sell(stock_info.code, stock_info.price, stock_info.counts) == f"[{api}] {stock_info.code}, {stock_info.price}, {stock_info.counts} sell success"
+
+
+def test_sell_nemo_api(stock_info):
+    api = "nemo"
+    driver = AutoTradingSystem(api, AutoTradingSystem)
+    driver.select_stock_brocker(api)
+    assert driver.sell(stock_info.code, stock_info.price, stock_info.counts) == f"[{api}] {stock_info.code}, {stock_info.price}, {stock_info.counts} sell success"
+
+
+def test_buy_nemo_mock(mocker: MockerFixture, stock_info):
+    driver = mocker.Mock(spec=StockBrokerDriverInterface)
+    api = "nemo"
+    auto_trading_system = AutoTradingSystem(api, driver)
+    driver.buy.return_value = True
+    assert auto_trading_system.buy(stock_info.code, stock_info.price,
+                                    stock_info.counts) == f"[{api}] {stock_info.code}, {stock_info.price}, {stock_info.counts} buy success"
+
+
+def test_buy_kiwer_mock(mocker: MockerFixture, stock_info):
+    driver = mocker.Mock(spec=StockBrokerDriverInterface)
+    api = "kiwer"
+    auto_trading_system = AutoTradingSystem(api, driver)
+    driver.buy.return_value = True
+    assert auto_trading_system.buy(stock_info.code, stock_info.price,
+                                    stock_info.counts) == f"[{api}] {stock_info.code}, {stock_info.price}, {stock_info.counts} buy success"
+
+
+def test_buy_kiwer_api(stock_info):
+    api = "kiwer"
+    driver = AutoTradingSystem(api, AutoTradingSystem)
+    driver.select_stock_brocker(api)
+    assert driver.buy(stock_info.code, stock_info.price,
+                       stock_info.counts) == f"[{api}] {stock_info.code}, {stock_info.price}, {stock_info.counts} buy success"
+
+
+def test_buy_nemo_api(stock_info):
+    api = "nemo"
+    driver = AutoTradingSystem(api, AutoTradingSystem)
+    driver.select_stock_brocker(api)
+    assert driver.buy(stock_info.code, stock_info.price,
+                       stock_info.counts) == f"[{api}] {stock_info.code}, {stock_info.price}, {stock_info.counts} buy success"
+
+def test_get_price_nemo_mock(mocker: MockerFixture, stock_info):
+    driver = mocker.Mock(spec=StockBrokerDriverInterface)
+    api = "nemo"
+    auto_trading_system = AutoTradingSystem(api, driver)
+    driver.get_price.return_value = 30000
+    assert auto_trading_system.buy(stock_info.code) == 30000
+
+
+def test_get_price_kiwer_mock(mocker: MockerFixture, stock_info):
+    driver = mocker.Mock(spec=StockBrokerDriverInterface)
+    api = "kiwer"
+    auto_trading_system = AutoTradingSystem(api, driver)
+    driver.get_price.return_value = True
+    assert auto_trading_system.get_price(stock_info.code) == 30000
+
+def test_get_price_kiwer_api(stock_info):
+    api = "kiwer"
+    driver = AutoTradingSystem(api, AutoTradingSystem)
+    driver.select_stock_brocker(api)
+    assert driver.get_price(stock_info.code) == 30000
+
+
+def test_get_price_nemo_api(stock_info):
+    api = "nemo"
+    driver = AutoTradingSystem(api, AutoTradingSystem)
+    driver.select_stock_brocker(api)
+    assert driver.get_price(stock_info.code) == 30000
